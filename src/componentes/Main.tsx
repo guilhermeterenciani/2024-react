d41a92ba32e4600963a9b1c835473653ad7b580f
Guilherme Figueiredo Terenciani <guilhermeterenciani@gmail.com>
2024-03-19 10:12:48 -0400

    Box-estudante main.tsx

--- src/componentes/Main.tsx
+++ src/componentes/Main.tsx
@@ -6,8 +6,8 @@ function Main(){
                 <img className='estudante-foto' src="https://github.com/guilhermeterenciani.png" alt=""/>
                 <div className="informacoes">
                     <h3 className="estudante_nome">Guilherme Figueiredo Terenciani</h3>
-                    <p className="estudante_descricao">Professor IFMS campus Naviraí na área de desenvolvimento/desenvolvimento web</p>
-                    <a href="#">GitHub</a>
+                    <p className="estudante_descricao">Professor IFMS campus Naviraí na área de desenvolvimento / desenvolvimento web</p>
+                    <a className='link-git' href="https://github.com/guilhermeterenciani">GitHub</a>
                 </div>
             </div>
         </main>
